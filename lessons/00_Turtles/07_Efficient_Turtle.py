1bbd66a164fd1df7e799d20da07b6b84b3cff296
EnochPrograms <enochprograms@gmail.com>
2025-03-12 21:56:46 +0000

    did stuff

--- lessons/00_Turtles/07_Efficient_Turtle.py
+++ lessons/00_Turtles/07_Efficient_Turtle.py
@@ -15,24 +15,24 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle
 tina.speed(2)                           # Make the turtle move as fast, but not too fast. 
 
-def draw_polygon(sides):
+def draw_polygon(sides, x=0, y=0):
 
-    angle = ... # Calculate angle from number of sides
+    tina.penup()                                     # Move tina to another spot on the screen
+    tina.goto(x,y)
+    tina.pendown()
+
+    angle = 360/sides                           # Calculate angle from number of sides
     
-    for i in range(...):                 # Loop through the number of sides
-        ...                              # Move tina forward by the forward distance
-        ...                              # Turn tina left by the left turn
+    for i in range(sides):                      # Loop through the number of sides
+        tina.forward(100)                              # Move tina forward by the forward distance
+        tina.right(angle)                              # Turn tina left by the left turn
 
 
-draw_polygon(...)                        # Draw a square
+draw_polygon(4)                                  # Draw a square
 
-...                                      # Move tina to another spot on the screen
-
-draw_polygon(...)                        # Draw a pentagon
-
-...                                      # Move tina to another spot on the screen
-
-draw_polygon(...)                        # Draw a hexagon
+draw_polygon(5,0,20)                        # Draw a pentagon
+                                      # Move tina to another spot on the screen
+draw_polygon(10,-200,0)                        # Draw a hexagon
 
 
 turtle.exitonclick()                     # Close the window when we click on it
--- lessons/00_Turtles/08c_More_Turtle_Programs.py
+++ lessons/00_Turtles/08c_More_Turtle_Programs.py
@@ -7,3 +7,85 @@ from the section "Change the Background Image"
 Then change the code so that the turtle has a different image ( look in the 'images'
 directory ) and moves to the corners of the screen in a square pattern. 
 """
+
+
+import turtle
+
+def set_background_image(window, image_name):
+
+    from pathlib import Path
+    from PIL import Image
+
+
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+
+# Set up the screen
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(width=600, height=600)     # Set the size of the window
+
+tina = turtle.Turtle()                  # Create a turtle named tina
+
+screen = turtle.Screen()                # Get the screen that tina is on
+set_background_image(screen, "emoji2.png") # Set the background image of the screen
+
+import turtle
+
+
+def set_turtle_image(turtle, image_name):
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+
+t = turtle.Turtle()
+
+set_turtle_image(t, "boy_yellow.gif")
+
+t.penup()
+t.speed(3)
+
+for i in range(4):
+    t.goto(200, 200)
+    t.goto(-200, -200)
+
+
+turtle.exitonclick()          
+
+import turtle
+
+def set_background_image(window, image_name):
+
+    from pathlib import Path
+    from PIL import Image
+
+
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+
+# Set up the screen
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(width=600, height=600)     # Set the size of the window
+
+tina = turtle.Turtle()                  # Create a turtle named tina
+
+screen = turtle.Screen()                # Get the screen that tina is on
+set_background_image(screen, "emoji2.png") # Set the background image of the screen
+
