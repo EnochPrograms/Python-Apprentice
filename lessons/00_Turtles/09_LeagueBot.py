6c429b8bbcab51dabba963fbfa44811247889ef1
EnochPrograms <enochprograms@gmail.com>
2025-03-26 14:57:30 -0700

    I added an image to leaguebot
    yeet

--- lessons/00_Turtles/09_LeagueBot.py
+++ lessons/00_Turtles/09_LeagueBot.py
@@ -17,4 +17,19 @@ screen.bgcolor('white')
 
 t = turtle.Turtle()
 
-... # Your Code Here
+def set_turtle_image(turtle, image_name):
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+set_turtle_image(t, "leaguebot_bolt.gif")
+t.turtlesize(10,10)
+t.pencolor('blue')
+
+
+turtle.exitonclick()
