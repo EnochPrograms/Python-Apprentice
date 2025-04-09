66ec510e79f64ffd2410f40e1455641c5d285af9
EnochPrograms <enochprograms@gmail.com>
2025-04-09 14:25:07 -0700

    We made the mustache go to the mouse
    yeet

--- lessons/00_Turtles/10b_Tash_Me_Click.py
+++ lessons/00_Turtles/10b_Tash_Me_Click.py
@@ -5,5 +5,47 @@
 #
 # Hint: See 08a_More Turtle Programs, section 'Click on the Screen'
  
-... # Your code here
 
+import turtle
+def set_background_image(window, image_name):
+    from pathlib import Path
+    from PIL import Image
+
+
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+    image = Image.open(image_path)
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+
+turtle.setup(width=600, height=600)
+tina = turtle.Turtle()
+screen = turtle.Screen()
+set_background_image(screen, "emoji.png")
+
+
+def set_turtle_image(turtle, image_name):
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+set_turtle_image(tina, "moustache3.gif")
+tina.penup()
+tina.goto(x=0, y=215)
+
+tom=turtle.Turtle()
+set_turtle_image(tom, "moustache1.gif")
+
+tom.penup()
+
+def screen_clicked(x, y):
+    print('You pressed X=' + str(x) + ' , y=' + str(y))
+    tom.goto(x, y)
+screen.onclick(screen_clicked)
+
+turtle.done()
--- lessons/00_Turtles/10c_Tash_Me_Twirl.py
+++ lessons/00_Turtles/10c_Tash_Me_Twirl.py
@@ -7,5 +7,47 @@ so the moustache will twirl when you click on it.
 Hint: See 08a_More Turtle Programs, section 'Click on the Turtle'
 """
 
-... # Your code here
+import turtle
+def set_background_image(window, image_name):
+    from pathlib import Path
+    from PIL import Image
+
+
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+    image = Image.open(image_path)
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+
+turtle.setup(width=600, height=600)
+tina = turtle.Turtle()
+screen = turtle.Screen()
+set_background_image(screen, "emoji.png")
+
+
+def set_turtle_image(turtle, image_name):
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+set_turtle_image(tina, "moustache3.gif")
+tina.penup()
+tina.goto(x=0, y=215)
+
+tom=turtle.Turtle()
+set_turtle_image(tom, "moustache1.gif")
+
+def turtle_clicked(x, y):
+    print('X=' + str(x) + ' , y=' + str(y))
+    for i in range(0, 360, 20):
+        tom.left(20)
+tom.onclick(turtle_clicked)
+
+turtle.done()
+
 
